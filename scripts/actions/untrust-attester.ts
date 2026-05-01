1c9317204fb4ca0a287657f46c1cd7879260a8b9
Kyle Thornton <kyle@cowrie.io>
2026-04-30 18:40:33 -0700

    feat: untrust-attester script (setAttesterTrust false)

---
+++ scripts/actions/untrust-attester.ts
@@ -0,0 +1,25 @@
+import {
+  connectViem,
+  getResolverContract,
+  loadResolverDeployment,
+  logSuccess,
+  parseAddressList,
+  requireOption,
+  resolveOption,
+  resolveWallet,
+} from "../utils/resolver.js";
+
+const accountsArg = requireOption("--accounts", ["ACCOUNTS", "RESOLVER_ACCOUNTS"]);
+const fromArg = resolveOption("--from", ["FROM"]);
+
+const accounts = parseAddressList(accountsArg, "--accounts");
+
+const { viem, chainId } = await connectViem();
+const wallet = await resolveWallet(viem, fromArg);
+const deployment = await loadResolverDeployment(chainId);
+const resolver = await getResolverContract(viem, deployment.address, wallet);
+
+for (const account of accounts) {
+  await resolver.write.setAttesterTrust([account, false]);
+  logSuccess("Attester untrusted", { resolver: deployment.address, attester: account });
+}
