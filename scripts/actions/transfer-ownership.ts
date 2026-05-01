4966b0cf72102f47f767904f00cbe968235098ab
Kyle Thornton <kyle@cowrie.io>
2026-04-30 18:54:23 -0700

    refactor: transfer-ownership script targets SanctionsResolver

--- scripts/actions/transfer-ownership.ts
+++ scripts/actions/transfer-ownership.ts
@@ -1,22 +1,20 @@
 import {
   connectViem,
-  getRegistryContract,
+  getResolverContract,
+  loadResolverDeployment,
+  logSuccess,
   requireOption,
   resolveOption,
   resolveWallet,
-  logSuccess,
-} from "../utils/registry.js";
+} from "../utils/resolver.js";
 
-const registryAddress = requireOption("--registry", ["REGISTRY", "REGISTRY_ADDRESS"]);
-const newOwner = requireOption("--new-owner", ["NEW_OWNER", "REGISTRY_NEW_OWNER"]);
-const from = resolveOption("--from", ["FROM", "REGISTRY_FROM"]);
+const newOwner = requireOption("--new-owner", ["NEW_OWNER", "RESOLVER_NEW_OWNER"]);
+const fromArg = resolveOption("--from", ["FROM"]);
 
-const { viem } = await connectViem();
-const wallet = await resolveWallet(viem, from);
-const registry = await getRegistryContract(viem, registryAddress, wallet);
+const { viem, chainId } = await connectViem();
+const wallet = await resolveWallet(viem, fromArg);
+const deployment = await loadResolverDeployment(chainId);
+const resolver = await getResolverContract(viem, deployment.address, wallet);
 
-await registry.write.transferOwnership([newOwner]);
-logSuccess("Transferred ownership", {
-  registry: registryAddress,
-  newOwner,
-});
+await resolver.write.transferOwnership([newOwner as `0x${string}`]);
+logSuccess("Transferred ownership", { resolver: deployment.address, newOwner });
